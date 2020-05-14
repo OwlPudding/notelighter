ff05035c118987c2ca10326a253d98f0af4b5928
OwlPudding <acortesose@gmail.com>
2020-05-13 23:15:25 -0400

    Updates

---
+++ precache-manifest.31ca436c18dcf79da212cbfb67faab92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486bc5563d6503c1b6b9e1b54e18c432",
+    "url": "/notelighter/c54c338c088dfe9af1a8.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/notelighter/c54c338c088dfe9af1a8.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3448c73004219bbc2d5bbe77f050c4",
+    "url": "/notelighter/index.html"
+  },
+  {
+    "revision": "84dba03649d51fa25555",
+    "url": "/notelighter/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "fe72c2b97d477ecdaf01",
+    "url": "/notelighter/static/css/main.7ae5cb59.chunk.css"
+  },
+  {
+    "revision": "84dba03649d51fa25555",
+    "url": "/notelighter/static/js/2.c8e3ac71.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/notelighter/static/js/2.c8e3ac71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe72c2b97d477ecdaf01",
+    "url": "/notelighter/static/js/main.6ca2346a.chunk.js"
+  },
+  {
+    "revision": "4261f0d898fc18304f57",
+    "url": "/notelighter/static/js/runtime-main.52d8dd34.js"
+  }
+]);
